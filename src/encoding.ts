ac586ebe7e28421631114c2abee6c20b4685de5b
KiraLT <KiraLT@users.noreply.github.com>
2021-06-06 19:16:29 +0300

    base64 functions better support

--- src/encoding.ts
+++ src/encoding.ts
@@ -37,27 +37,111 @@ export function generateUUID(): string {
     )
 }
 
+const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
 /**
  * Encodes given value with [Base64](https://en.wikipedia.org/wiki/Base64) algorithm.
  */
-export function base64Decode(value: string): string {
-    return decodeURIComponent(
-        atob(value)
-            .split('')
-            .map((c) => {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-            })
-            .join('')
-    )
+export function base64Decode(input: string): string {
+    let output = ''
+    let i = 0
+
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '')
+
+    while (i < input.length) {
+        const enc1 = keyStr.indexOf(input.charAt(i++))
+        const enc2 = keyStr.indexOf(input.charAt(i++))
+        const enc3 = keyStr.indexOf(input.charAt(i++))
+        const enc4 = keyStr.indexOf(input.charAt(i++))
+
+        const chr1 = (enc1 << 2) | (enc2 >> 4)
+        const chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+        const chr3 = ((enc3 & 3) << 6) | enc4
+
+        output = output + String.fromCharCode(chr1)
+
+        if (enc3 !== 64) {
+            output = output + String.fromCharCode(chr2)
+        }
+        if (enc4 !== 64) {
+            output = output + String.fromCharCode(chr3)
+        }
+    }
+
+    output = decodeUTF8(output)
+
+    return output
 }
 
 /**
  * Decodes [Base64](https://en.wikipedia.org/wiki/Base64) encoded value.
  */
-export function base64Encode(value: string): string {
-    return btoa(
-        encodeURIComponent(value).replace(/%([0-9A-F]{2})/g, (_, p1) => {
-            return String.fromCharCode(`0x${p1}` as any)
-        })
-    )
+export function base64Encode(input: string): string {
+    let output = ''
+    let i = 0
+    input = encodeUTF8(input)
+    while (i < input.length) {
+        const chr1 = input.charCodeAt(i++)
+        const chr2 = input.charCodeAt(i++)
+        const chr3 = input.charCodeAt(i++)
+        const enc1 = chr1 >> 2
+        const enc2 = ((chr1 & 3) << 4) | (chr2 >> 4)
+        let enc3 = ((chr2 & 15) << 2) | (chr3 >> 6)
+        let enc4 = chr3 & 63
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64
+        } else if (isNaN(chr3)) {
+            enc4 = 64
+        }
+        output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4)
+    }
+    return output
+}
+
+function encodeUTF8(input: string): string {
+    input = input.replace(/\r\n/g, '\n')
+    let utf8Text = ''
+
+    for (let n = 0; n < input.length; n++) {
+        const c = input.charCodeAt(n)
+
+        if (c < 128) {
+            utf8Text += String.fromCharCode(c)
+        } else if (c > 127 && c < 2048) {
+            utf8Text += String.fromCharCode((c >> 6) | 192)
+            utf8Text += String.fromCharCode((c & 63) | 128)
+        } else {
+            utf8Text += String.fromCharCode((c >> 12) | 224)
+            utf8Text += String.fromCharCode(((c >> 6) & 63) | 128)
+            utf8Text += String.fromCharCode((c & 63) | 128)
+        }
+    }
+
+    return utf8Text
+}
+
+function decodeUTF8(utf8Text: string): string {
+    let str = ''
+    let i = 0
+    let c1 = 0
+    let c2 = 0
+    let c3 = 0
+    while (i < utf8Text.length) {
+        c1 = utf8Text.charCodeAt(i)
+
+        if (c1 < 128) {
+            str += String.fromCharCode(c1)
+            i++
+        } else if (c1 > 191 && c1 < 224) {
+            c2 = utf8Text.charCodeAt(i + 1)
+            str += String.fromCharCode(((c1 & 31) << 6) | (c2 & 63))
+            i += 2
+        } else {
+            c2 = utf8Text.charCodeAt(i + 1)
+            c3 = utf8Text.charCodeAt(i + 2)
+            str += String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63))
+            i += 3
+        }
+    }
+    return str
 }
